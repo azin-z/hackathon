fee0a7d414c087994e748f898c291813ccbfbaa5
ozzyn13 <azin.zahrayi@gmail.com>
2020-02-20 19:20:00 +0100

    second commit

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 
+task = n
 file_name = 'a_example.txt'
 with open(file_name) as input_f:
     lines = input_f.readlines()
@@ -7,14 +8,36 @@ for (i, line) in lines:
     if i==0:
         words = line.split()
         num_of_books = words[0]
+        num_of_libraries = words[1]
+        scanning_days = words[2]
+        break
+    if i==1:
+
+
+
+class Task:
+    def __init__(self, num_of_books, sign_up_duration, books_per_day_shipping, book_list):
+        self.num_of_books = num_of_books
+        self.num_of_libraries = num_of_libraries
+        self.scanning_days = scanning_days
+        self.libraries = libraries
+        self.books_scores = book_scores
+
+class Solution:
+    def __init__(self, lib_ordering):
+        self.lib_ordering = lib_ordering
+        self.books_per_library = []
+        self.scores = None
 
 
 
 class Library:
-    num_of_books = 0
 
-    def __init__(self, num_of_books, sign_up_duration, books_per_day_shipping, book_indexes):
+    def __init__(self, num_of_books, sign_up_duration, books_per_day_shipping, book_list):
         self.num_of_books = num_of_books
         self.sign_up_duration = sign_up_duration
         self.books_per_day_shipping = books_per_day_shipping
-        self.book_indexes = book_indexes
+        self.book_list = book_list
+
+
+def
