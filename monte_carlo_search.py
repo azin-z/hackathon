ec85e52a37f0c85647c52086e914406246a08f27
Mikko Rautiainen <m_rautiainen@hotmail.com>
2020-02-20 21:23:11 +0100

    multithreaded monte carlo search

--- monte_carlo_search.py
+++ monte_carlo_search.py
@@ -7,11 +7,20 @@ import parsing
 import sys
 import random
 import subprocess
+import time
+import threading
+import queue
 
-input_problem_pickle = sys.argv[1]
-output_solution_pickle = sys.argv[2]
+num_threads = int(sys.argv[1])
+input_problem_pickle = sys.argv[2]
+output_solution_pickle = sys.argv[3]
+
+print(num_threads)
 print(input_problem_pickle)
 print(output_solution_pickle)
+
+initial_best_solution = None
+solution_queue = queue.Queue(0)
 
 def generate_libray_permutation(n):
 	order = []
@@ -20,24 +29,53 @@ def generate_libray_permutation(n):
 	random.shuffle(order)
 	return order
 
+def run_montecarlo():
+	best_here = initial_best_solution
+	while True:
+		new_order = generate_libray_permutation(len(task.libraries))
+		new_solution = classes.Solution(new_order)
+		score_solution.fill_books_and_score(task, new_solution)
+		if new_solution.scores <= best_here.scores: continue
+		with open(output_solution_pickle + ".tmp", "wb") as pickle_out:
+			pickle.dump(new_solution, pickle_out)
+		best_here = new_solution
+		solution_queue.put(best_here)
+
+def run_pick_best():
+	global_best = initial_best_solution
+	while True:
+		new_solution = solution_queue.get()
+		if new_solution.scores <= global_best.scores: continue
+		with open(output_solution_pickle + ".tmp", "wb") as pickle_out:
+			pickle.dump(new_solution, pickle_out)
+		global_best = new_solution
+		subprocess.run(["cp", output_solution_pickle + ".tmp", output_solution_pickle])
+		subprocess.run(["rm", output_solution_pickle + ".tmp"])
+		print("found solution with score: " + str(global_best.scores))
+
 with open(input_problem_pickle, "rb") as pickle_in:
 	task = pickle.load(pickle_in)
 
 try:
 	with open(output_solution_pickle, "rb") as pickle_in:
-		best_solution = pickle.load(pickle_in)
+		initial_best_solution = pickle.load(pickle_in)
 except:
-	best_solution = classes.Solution([0])
-	best_solution.scores = -1
+	initial_best_solution = classes.Solution([0])
+	initial_best_solution.scores = -1
+
+start_time = time.time()
+new_order = generate_libray_permutation(len(task.libraries))
+new_solution = classes.Solution(new_order)
+score_solution.fill_books_and_score(task, new_solution)
+end_time = time.time()
+print("One iteration takes " + str(end_time - start_time) + " seconds")
+
+write_thread = threading.Thread(target=run_pick_best)
+write_thread.start()
+
+for i in range(0, num_threads):
+	threadi = threading.Thread(target=run_montecarlo)
+	threadi.start()
 
 while True:
-	new_order = generate_libray_permutation(len(task.libraries))
-	new_solution = classes.Solution(new_order)
-	score_solution.fill_books_and_score(task, new_solution)
-	if new_solution.scores <= best_solution.scores: continue
-	with open(output_solution_pickle + ".tmp", "wb") as pickle_out:
-		pickle.dump(new_solution, pickle_out)
-	best_solution = new_solution
-	subprocess.run(["cp", output_solution_pickle + ".tmp", output_solution_pickle])
-	subprocess.run(["rm", output_solution_pickle + ".tmp"])
-	print("found solution with score: " + str(best_solution.scores))
+	pass # loop until closed
--- score_solution.py
+++ score_solution.py
@@ -8,25 +8,16 @@ def fill_books_and_score(task, solution):
 	current_day = 0
 	library_nodes = {}
 	book_node = {}
-	start_nodes = []
-	end_nodes = []
-	capacities = []
-	unit_costs = []
 	source_node = 0
 	sink_node = 1
 	next_node = 2
 	supplies = []
-	start_nodes.append(sink_node)
-	end_nodes.append(source_node)
-	capacities.append(len(task.book_scores))
-	unit_costs.append(0)
+	min_cost_flow = pywrapgraph.SimpleMinCostFlow()
+	min_cost_flow.AddArcWithCapacityAndUnitCost(sink_node, source_node, len(task.book_scores), 0)
 	for book in range(len(task.book_scores)):
 		book_node[book] = next_node
 		next_node += 1
-		start_nodes.append(book_node[book])
-		end_nodes.append(sink_node)
-		capacities.append(1)
-		unit_costs.append(-task.book_scores[book])
+		min_cost_flow.AddArcWithCapacityAndUnitCost(book_node[book], sink_node, 1, -task.book_scores[book])
 	for lib in solution.lib_ordering:
 		current_day += task.libraries[lib].sign_up_duration
 		if current_day > task.scanning_days: break
@@ -34,15 +25,9 @@ def fill_books_and_score(task, solution):
 		if books_from_here <= 0: break
 		library_nodes[lib] = next_node
 		next_node += 1
-		start_nodes.append(source_node)
-		end_nodes.append(library_nodes[lib])
-		capacities.append(books_from_here)
-		unit_costs.append(0)
+		min_cost_flow.AddArcWithCapacityAndUnitCost(source_node, library_nodes[lib], books_from_here, 0)
 		for book in task.libraries[lib].book_list:
-			start_nodes.append(library_nodes[lib])
-			end_nodes.append(book_node[book])
-			capacities.append(1)
-			unit_costs.append(0)
+			min_cost_flow.AddArcWithCapacityAndUnitCost(library_nodes[lib], book_node[book], 1, 0)
 	for i in range(0, next_node):
 		supplies.append(0)
 	rev_library_node = {}
@@ -51,15 +36,8 @@ def fill_books_and_score(task, solution):
 		rev_library_node[library_nodes[k]] = k
 	for k in book_node:
 		rev_book_node[book_node[k]] = k
-	min_cost_flow = pywrapgraph.SimpleMinCostFlow()
-	for i in range(0, len(start_nodes)):
-		min_cost_flow.AddArcWithCapacityAndUnitCost(start_nodes[i], end_nodes[i], capacities[i], unit_costs[i])
 	for i in range(0, len(supplies)):
 		min_cost_flow.SetNodeSupply(i, supplies[i])
-	assert len(start_nodes) == len(end_nodes)
-	assert len(end_nodes) == len(capacities)
-	assert len(capacities) == len(unit_costs)
-	assert len(supplies) == next_node
 	solved = min_cost_flow.SolveMaxFlowWithMinCost()
 	if solved != min_cost_flow.OPTIMAL:
 		print("error solving the flow?")
