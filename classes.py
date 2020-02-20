b562373fabd4bad940973b06c7ba6c182e5a2727
ozzyn13 <azin.zahrayi@gmail.com>
2020-02-20 19:22:14 +0100

    name corrections

--- classes.py
+++ classes.py
@@ -1,12 +1,12 @@
 
 
 class Task:
-    def __init__(self, num_of_books, sign_up_duration, books_per_day_shipping, book_list):
+    def __init__(self, num_of_books, num_of_libraries, scanning_days, libraries, book_scores):
         self.num_of_books = num_of_books
         self.num_of_libraries = num_of_libraries
         self.scanning_days = scanning_days
         self.libraries = libraries
-        self.books_scores = book_scores
+        self.book_scores = book_scores
 
 class Solution:
     def __init__(self, lib_ordering):
@@ -15,14 +15,10 @@ class Solution:
         self.scores = None
 
 
-
 class Library:
-
     def __init__(self, num_of_books, sign_up_duration, books_per_day_shipping, book_list):
         self.num_of_books = num_of_books
         self.sign_up_duration = sign_up_duration
         self.books_per_day_shipping = books_per_day_shipping
         self.book_list = book_list
 
-
-def
